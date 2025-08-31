4b897c6efe32de647bee22a6246f2ead8c3481d0
Smera S Kini <smeraskini@gmail.com>
2025-08-31 22:09:48 +0530

    Initial commit on dataset.h file

---
+++ dataset.h
@@ -0,0 +1,28 @@
+
+#ifndef DATASET_H
+#define DATASET_H
+
+#include <vector>
+#include <string>
+
+struct Sample {
+    std::vector<float> img;
+    int label;
+};
+
+std::vector<Sample> load_folder_samples(const std::string &folder, int label);
+std::vector<Sample> build_dataset_samples(const std::string &base_folder);
+void shuffle_dataset(std::vector<Sample> &ds);
+
+#endif
+
+// training.h
+#ifndef TRAINING_H
+#define TRAINING_H
+
+#include "model.h"
+#include "dataset.h"
+
+float evaluate(SimpleCNN &model, const std::vector<Sample>& val);
+
+#endif
